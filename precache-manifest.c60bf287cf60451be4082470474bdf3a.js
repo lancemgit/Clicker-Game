f7293b278b131dab3ea2967e0aac7eb537ff2ba1
Lance Meara <meara.ty@gmail.com>
2020-02-08 14:10:49 -0600

    Updates

---
+++ precache-manifest.c60bf287cf60451be4082470474bdf3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c2472ddd7bbd112cee2ddf17edb87e",
+    "url": "/Clicker-Game/index.html"
+  },
+  {
+    "revision": "57d64c36a76c170cd5e0",
+    "url": "/Clicker-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d490c55ff87f4ef46372",
+    "url": "/Clicker-Game/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "57d64c36a76c170cd5e0",
+    "url": "/Clicker-Game/static/js/2.86fe16d0.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/Clicker-Game/static/js/2.86fe16d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d490c55ff87f4ef46372",
+    "url": "/Clicker-Game/static/js/main.1eae1547.chunk.js"
+  },
+  {
+    "revision": "e8bee6a40d667c87fad5",
+    "url": "/Clicker-Game/static/js/runtime-main.5c6a5bf3.js"
+  }
+]);
